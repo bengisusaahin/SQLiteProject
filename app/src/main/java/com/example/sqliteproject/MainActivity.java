2eb6b47afa3bdf3cfac18c0cb7a208e10a9d8b44
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-09-03 15:33:24 +0300

    Created SQLite database

--- app/src/main/java/com/example/sqliteproject/MainActivity.java
+++ app/src/main/java/com/example/sqliteproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sqliteproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            SQLiteDatabase database = this.openOrCreateDatabase("Musicians", MODE_PRIVATE,
+                    null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT)");
+            
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
