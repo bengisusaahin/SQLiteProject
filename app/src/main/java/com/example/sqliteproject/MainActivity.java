805b6ebe3ddf67dbf0f8f6c972d1ffbbe82553fd
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-09-03 15:44:54 +0300

    Added data to the database

--- app/src/main/java/com/example/sqliteproject/MainActivity.java
+++ app/src/main/java/com/example/sqliteproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sqliteproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
@@ -16,7 +17,20 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase database = this.openOrCreateDatabase("Musicians", MODE_PRIVATE,
                     null);
             database.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT)");
-            
+
+//            database.execSQL("INSERT INTO musicians (name,age) VALUES ('James',50)");
+            database.execSQL("INSERT INTO musicians (name,age) VALUES ('Lars',60)");
+
+            Cursor cursor = database.rawQuery("SELECT * FROM musicians",null);
+
+            int nameIndex = cursor.getColumnIndex("name");
+            int ageIx = cursor.getColumnIndex("age");
+
+            while (cursor.moveToNext()){
+                System.out.println("Name: "+ cursor.getString(nameIndex));
+                System.out.println("Age: "+ cursor.getInt(ageIx));
+            }
+            cursor.close();
         }catch (Exception e){
             e.printStackTrace();
         }
